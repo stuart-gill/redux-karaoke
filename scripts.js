696dd08c0ac54f6eff44036c21c5d6c851573e46
Stuart Gill <stuart.a.gill@gmail.com>
2019-02-24 21:03:59 -0800

    complete interface logic

--- scripts.js
+++ scripts.js
@@ -37,9 +37,36 @@ expect(reducer(initialState, { type: "NEXT_LYRIC" })).toEqual({
 // REDUX STORE
 const { createStore } = Redux;
 const store = createStore(reducer);
-console.log(store.getState());
+
+// RENDERING STATE IN DOM
+const renderLyrics = () => {
+  // defines a lyricsDisplay constant referring to the div with a 'lyrics' ID in index.html
+  const lyricsDisplay = document.getElementById("lyrics");
+  // if there are already lyrics in this div, remove them one-by-one until it is empty:
+  while (lyricsDisplay.firstChild) {
+    lyricsDisplay.removeChild(lyricsDisplay.firstChild);
+  }
+  // Locates the song lyric at the current arrayPosition:
+  const currentLine = store.getState().songLyricsArray[
+    store.getState().arrayPosition
+  ];
+  // Creates DOM text node containing the song lyric identified in line above:
+  const renderedLine = document.createTextNode(currentLine);
+  // Adds text node created in line above to 'lyrics' div in DOM
+  document.getElementById("lyrics").appendChild(renderedLine);
+};
+
+// runs renderLyrics() method from above when page is finished loading.
+// window.onload is HTML5 version of jQuery's $(document).ready()
+window.onload = function() {
+  renderLyrics();
+};
 
 // CLICK LISTENER
 const userClick = () => {
-  console.log("click");
+  store.dispatch({ type: "NEXT_LYRIC" });
+  console.log(store.getState());
 };
+
+// SUBSCRIBE TO REDUX STORE
+store.subscribe(renderLyrics);
